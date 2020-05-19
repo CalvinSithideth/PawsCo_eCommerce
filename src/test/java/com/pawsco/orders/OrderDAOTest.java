650ba21febc240d73fc868c6679856fe844b2fb2
Cole Hamilton <cole.hamilton@ust-global.com>
2020-05-19 14:01:52 -0700

    Added test cases

---
+++ src/test/java/com/pawsco/orders/OrderDAOTest.java
@@ -0,0 +1,49 @@
+package com.pawsco.orders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OrderDAOTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSetDataSource() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testListOrders() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCreateOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
