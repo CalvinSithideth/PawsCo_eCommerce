aa3ef3bbb5378b8dd5d381e5d3a239478f45678f
jkjohnston <jkjohnston94@gmail.com>
2020-06-01 10:57:49 -0700

    registration page with address and functionality

--- PawsCo_eCommerce/src/main/java/com/pawsco/controllers/AccountController.java
+++ PawsCo_eCommerce/src/main/java/com/pawsco/controllers/AccountController.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.pawsco.business.Address;
 import com.pawsco.business.Order;
 import com.pawsco.business.User;
 import com.pawsco.db.orders.OrderJDBCTemplate;
+import com.pawsco.db.users.AddressJDBCTemplate;
 import com.pawsco.db.users.UserJDBCTemplate;
 
 @Controller
@@ -33,103 +35,72 @@ public class AccountController extends HttpServlet {
 	@Autowired
 	public UserJDBCTemplate userDB;
 	@Autowired
-	OrderJDBCTemplate orderDB;
+	public OrderJDBCTemplate orderDB;
 	@Autowired
 	public Order order;
+	@Autowired
+	public AddressJDBCTemplate addressDB;
+	@Autowired
+	public Address address;
 
-	@RequestMapping(method = RequestMethod.GET)
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-
-		// get current action
-		String action = request.getParameter("action");
-		if (action == null) {
-			action = "home"; // default action
-		}
-
-		// perform action and set URL to appropriate page
-		String url = "/home";
-		if (action.equals("home")) {
-			url = "/home";
-		} else if (action.equals("registerUser")) {
-			try {
-				url = registerUser(request, response);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if (action.equals("logout")) {
-			url = logoutUser(request, response);
-		} else if (action.equals("viewCookies")) {
-
-			url = "/view_cookies.jsp";
-		} else if (action.equals("deleteCookies")) {
-			url = deleteCookies(request, response);
-		}
-		// forward to the view
-		getServletContext().getRequestDispatcher(url).forward(request, response);
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		String action = request.getParameter("action");
-
-		// perform action and set URL to appropriate page
-		String url = "/home";
-		if (action.equals("registerUser")) {
-			try {
-				url = registerUser(request, response);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		// forward to the view
-		response.sendRedirect("home");
-	}
-
-//	public String handleGetLogin(Model model) {
-//		model.addAttribute("signin", new User());
-//		return "signin";
+//	@RequestMapping(method = RequestMethod.GET)
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//			throws IOException, ServletException {
+//
+//		// get current action
+//		String action = request.getParameter("action");
+//		if (action == null) {
+//			action = "home"; // default action
+//		}
+//
+//		// perform action and set URL to appropriate page
+//		String url = "/home";
+//		if (action.equals("home")) {
+//			url = "/home";
+//		} else if (action.equals("registerUser")) {
+//			try {
+//				url = registerUser(request, response);
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		} else if (action.equals("logout")) {
+//			url = logoutUser(request, response);
+//		} else if (action.equals("viewCookies")) {
+//
+//			url = "/view_cookies.jsp";
+//		} else if (action.equals("deleteCookies")) {
+//			url = deleteCookies(request, response);
+//		}
+//		// forward to the view
+//		getServletContext().getRequestDispatcher(url).forward(request, response);
 //	}
 //
-//	@PostMapping(value = "login")
-//	public String handlePostLogin(HttpServletRequest request, HttpServletResponse response,
-//			@Valid @ModelAttribute("login") User user) throws SQLException, IOException {
+//	@RequestMapping(method = RequestMethod.POST)
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
 //
-//		return loginUser(request, response);
-//	}
+//		String action = request.getParameter("action");
 //
-//	public String handleGetLogout(Model model) {
-//		model.addAttribute("loggedOut", new User());
-//		return "loggedOut";
-//	}
-//
-//	@PostMapping(value = "logout")
-//	public String handlePostLogout(HttpServletRequest request, HttpServletResponse response,
-//			@Valid @ModelAttribute("logout") User user) throws SQLException, ServletException, IOException {
-//
-//		return logoutUser(request, response);
-//	}
-//
-//	public String handleGetRegistration(Model model) {
-//		model.addAttribute("register", new User());
-//		return "register";
-//	}
-//
-//	@PostMapping(value = "register")
-//	public String handlePostRegistration(HttpServletRequest request, HttpServletResponse response,
-//			@Valid @ModelAttribute("register") User user) throws SQLException {
-//
-//		return registerUser(request, response);
+//		// perform action and set URL to appropriate page
+//		String url = "/home";
+//		if (action.equals("registerUser")) {
+//			try {
+//				url = registerUser(request, response);
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		// forward to the view
+//		response.sendRedirect("home");
 //	}
 
 	@PostMapping(value = "register")
 	private String registerUser(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 		ApplicationContext context = new AnnotationConfigApplicationContext("com.pawsco");
 		userDB = context.getBean(UserJDBCTemplate.class);
+		addressDB = context.getBean(AddressJDBCTemplate.class);
 
 		String url = null;
 		String message = null;
@@ -138,13 +109,14 @@ public class AccountController extends HttpServlet {
 		String password = request.getParameter("password");
 		String firstname = request.getParameter("firstName");
 		String lastname = request.getParameter("lastName");
-
+		String street = request.getParameter("street");
+		String state = request.getParameter("state");
+		String city = request.getParameter("city");
+		String zip = request.getParameter("zip");
+		
 		if (userDB.userExists(email)) {
 			message = "This email address already exists. <br>" + "Please enter another email address.";
 			request.setAttribute("message", message);
-
-//			HttpSession session = request.getSession();
-//			session.setAttribute("user", user);
 
 			return "register";
 
@@ -153,8 +125,14 @@ public class AccountController extends HttpServlet {
 			user.setEmail(email);
 			user.setFirstName(firstname);
 			user.setLastName(lastname);
+			address.setStreet(street);
+			address.setState(state);
+			address.setCity(city);
+			address.setZip(zip);
 
 			userDB.registerUser(email, password, firstname, lastname);
+			addressDB.addAddress(email, street, city, state, zip);
+			
 			// store the User object as a session attribute
 			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
