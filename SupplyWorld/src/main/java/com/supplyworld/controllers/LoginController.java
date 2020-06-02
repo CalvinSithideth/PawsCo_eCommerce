ce0a97e37a2c4deff7106d6793d6d0be11552395
Calvin Sithideth <Calvin.Sithideth@gmail.com>
2020-06-02 11:31:36 -0700

    LoginController now sends order data to view

--- SupplyWorld/src/main/java/com/supplyworld/controllers/LoginController.java
+++ SupplyWorld/src/main/java/com/supplyworld/controllers/LoginController.java
@@ -1,10 +1,13 @@
 package com.supplyworld.controllers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.supplyworld.db.OrderJdbc;
 import com.supplyworld.services.LoginService;
 
 @Controller
@@ -15,6 +18,9 @@ public class LoginController {
 		ModelAndView mav = new ModelAndView();
 		if (LoginService.validateLogin(username, password)) {
 			mav.setViewName("orders");
+			ApplicationContext ctx = new AnnotationConfigApplicationContext("com.supplyworld");
+			OrderJdbc orderJdbc = ctx.getBean(OrderJdbc.class);
+			mav.addObject("orderlist", orderJdbc.listOrders());
 		} else {
 			mav.setViewName("index");
 		}
